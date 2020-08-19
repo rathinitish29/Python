9c67cc64af6468368ccb8b42d8a85944c5db957b
rathinitish29 <68941657+rathinitish29@users.noreply.github.com>
2020-08-19 15:49:02 +0530

    Add files via upload

---
+++ delete_methods_list.py
@@ -0,0 +1,14 @@
+# common methods to delete data from list
+fruit = ['mango', 'banana','apple','orange']
+print(fruit)
+#pop() method
+fruit.pop(1)
+print(fruit)
+#del[] operator
+del fruit[1]
+print(fruit)
+#remove()method
+fruit.remove('mango')
+print(fruit)
+#to add data = append,insert,extend
+#to delete = pop,del,remove
